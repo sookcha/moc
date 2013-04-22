11c8fc426894c003198aaa361085a57defd1cf85
sookcha <lsookcha@me.com>
2013-04-23 08:11:52 +0900

    Second.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Moc::Application.config.secret_token = 'c42e623e629edf008bbac105c9e00ded1eb01701c221f7df94cdfec9fad2c52c7c28075a437ed07e366cbbedb7db78194eb8db23c211d9c9580bfe7330559059'
