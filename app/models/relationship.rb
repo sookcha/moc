c2cb4e3fd53b2604997c5d66dfa0dac67f3f7f04
sookcha <lsookcha@me.com>
2013-05-13 20:59:20 +0900

    Adding following status

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -1,3 +1,9 @@
 class Relationship < ActiveRecord::Base
-  attr_accessible :followed_id, :follower_id
+  attr_accessible :following_id, :follower_id
+  
+  belongs_to :follower, class_name: "User"
+  belongs_to :following, class_name: "User"
+  
+  validates :follower_id, presence: true
+  validates :following_id, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,15 @@
 class User < ActiveRecord::Base
   has_many :link
   has_many :feed
+  
   has_many :relationships, foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :relationships, source: :following
+  
+  has_many :reverse_relationships, foreign_key: "following_id",
+                                   class_name:  "Relationship",
+                                   dependent:   :destroy
+  has_many :followers, through: :reverse_relationships, source: :follower
+  
   
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable,
@@ -11,5 +19,16 @@ class User < ActiveRecord::Base
 
   # Setup accessible (or protected) attributes for your model
   attr_accessible :email,:username, :password, :password_confirmation, :remember_me
-  # attr_accessible :title, :body
+  
+  def following?(other_user)
+    relationships.find_by_following_id(other_user.id)
+  end
+
+  def follow!(other_user)
+    relationships.create!(following_id: other_user.id)
+  end
+  def unfollow!(other_user)
+    relationships.find_by_following_id(other_user.id).destroy
+  end
+  
 end
