111c8d1abbbe95b4af61a46d1136c363879b65b0
sookcha <lsookcha@me.com>
2013-06-03 20:52:49 +0900

    Managing comments

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,7 @@
 class CommentsController < ApplicationController
+  def create
+    @link = Link.find(params[:link_id])
+    @comment = @link.comments.create(params[:comment])
+    redirect_to link_path(@link)
+  end
 end
