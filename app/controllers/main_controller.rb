9867302569c5ab3da259238f5694fc6d70d7cb73
sookcha <lsookcha@me.com>
2013-06-27 15:38:08 +0900

    페이지네이션 할때 아이템 배열이 올바름

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -3,7 +3,8 @@ class MainController < ApplicationController
     if user_signed_in?
       @followingUsers = current_user.following
       @allActivities = []
-      @feeds = Link.from_users_followed_by(current_user).page params[:page]
+      @feeds = Link.from_users_followed_by(current_user).reverse
+      @feeds_page = Kaminari.paginate_array(@feeds).page(params[:page]).per(20)
         
   		@followingUsers.each do |user|       
         @allActivities << user.links.all
